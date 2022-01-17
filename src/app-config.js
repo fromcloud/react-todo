1b0a554070a93f6684b6701155458b5bd0a3bc80
sw.han <outspace99@gmail.com>
2022-01-17 11:44:04 +0900

    Update app-config.js
    change http endpoint to https endpoint

--- src/app-config.js
+++ src/app-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === "localhost") {
 	backendHost = "http://localhost:8080";
 } else {
-  backendHost = "http://todoapp-env-1.eba-rkuzpf2r.ap-northeast-2.elasticbeanstalk.com";
+  backendHost = "https://todoapp-env-1.eba-rkuzpf2r.ap-northeast-2.elasticbeanstalk.com";
 }
 
 export const API_BASE_URL = `${backendHost}`;
